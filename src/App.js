8f17b391550d14ede55038eca0e3302ff7163013
wanko <kouji124@w4.dion.ne.jp>
2021-02-21 22:32:42 -0800

    class component 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {List} from './List'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {description: "Before click"}
+  }
+
+  changeDescription() {
+    this.setState({
+      description: "After click"
+    })
+  }
+
+  render() {
+    const {description} = this.state
+
+    return (
+      <div>
+        {description}
+        <List title="Languagessss" />
+        <button onClick={() => this.changeDescription()}>PRESS ME</button>
+      </div>
+    )
+  }
 }
 
 export default App;
